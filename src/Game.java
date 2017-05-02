ebb63dada55c40ca4c068bb05338363ec771c524
awesomea900 <awesomea900@gmail.com>
2017-05-02 22:34:34 +1000

    loaded level 1, from the assignment web page, it is actually impossible to solve. THEY CHEATED US!

--- src/Game.java
+++ src/Game.java
@@ -26,14 +26,35 @@ public class Game extends Canvas implements Runnable {
 		r = new Random();
 
 		// handler.addObject(new Player(0, 0, ID.Player));
-		handler.addObject(new GoalSquare(128, 128, ID.GoalSquare, handler));
+		handler.addObject(new GoalSquare(32 * 1, 32 * 5, ID.GoalSquare, handler));
+		handler.addObject(new GoalSquare(32 * 1, 32 * 6, ID.GoalSquare, handler));
+		handler.addObject(new GoalSquare(32 * 2, 32 * 6, ID.GoalSquare, handler));
+		handler.addObject(new GoalSquare(32 * 4, 32 * 6, ID.GoalSquare, handler));
 		
-		handler.addObject(new Player(0, 0, ID.Player2));
+		handler.addObject(new Player(32 * 1, 32 * 2, ID.Player2));
 		
-		handler.addObject(new Box(64, 64, ID.Box));
-		handler.addObject(new Box(32 * 10, 64, ID.Box));
-		handler.addObject(new Wall(32 * 8, 32 * 8, ID.Wall));
-		handler.addObject(new Wall(32 * 10, 32 * 8, ID.Wall));
+		handler.addObject(new Box(32 * 2, 32 * 2, ID.Box));
+		handler.addObject(new Box(32 * 2, 32 * 3, ID.Box));
+		handler.addObject(new Box(32 * 2, 32 * 5, ID.Box));
+		handler.addObject(new Box(32 * 3, 32 * 4, ID.Box));
+		
+		for(int i = 0; i < 6; i++){
+			handler.addObject(new Wall(32 * i, 32 * 0, ID.Wall));
+			handler.addObject(new Wall(32 * i, 32 * 7, ID.Wall));
+		}
+		for(int i = 1; i < 7; i++){
+			handler.addObject(new Wall(32 * 0, 32 * i, ID.Wall));
+			handler.addObject(new Wall(32 * 5, 32 * i, ID.Wall));
+		}
+		for(int i = 1; i < 4 ; i++){
+			handler.addObject(new Wall(32 * 4, 32 * i, ID.Wall));
+		}
+		
+		handler.addObject(new Wall(32 * 1, 32 * 1, ID.Wall));
+		handler.addObject(new Wall(32 * 1, 32 * 3, ID.Wall));
+		handler.addObject(new Wall(32 * 1, 32 * 4, ID.Wall));
+		handler.addObject(new Wall(32 * 3, 32 * 6, ID.Wall));
+		
 		
 		this.addKeyListener(new KeyInput(handler));
 
